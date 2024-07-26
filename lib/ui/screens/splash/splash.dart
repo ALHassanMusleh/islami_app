b6ebdd6629c3f3fde56d92ac43e21e4f237f5a92
Your Name <you@example.com>
2024-07-26 12:09:10 +0300

    Finished Splash Screen

---
+++ lib/ui/screens/splash/splash.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:islami_app/ui/screens/home/home.dart';
+import 'package:islami_app/ui/utilts/app_assets.dart';
+
+class Splash extends StatefulWidget {
+  static const String routeName = 'Splash';
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  // run initstate only one before build
+  @override
+  void initState() {
+    // TODO: implement initState
+    Future.delayed(
+      Duration(seconds: 2),
+      () {
+        Navigator.pushReplacementNamed(context, Home.routeName);
+      },
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('build'); // build method in emlator run 2 times
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(AppAssets.splash),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
